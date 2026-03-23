897ebbe8b3186aa6e0b2ea3d2095d98c30acf87a
Enzo Martellucci <enzomartellucci@gmail.com>
2026-03-23 22:48:16 +0100

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 // Import from the new modular architecture
 import { HomepageBuilder } from './pages'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
-  return <HomepageBuilder />
+  return (
+    <>
+      <HomepageBuilder />
+      <Analytics />
+    </>
+  )
 }
 
 export default App
